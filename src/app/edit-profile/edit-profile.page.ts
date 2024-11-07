a1366cc329290e9fa3a54254746c17ac0ed38019
Ch3l0s <ma.boetto@duocuc.cl>
2024-11-07 08:45:46 -0300

    Luis hizo edit profile

--- src/app/edit-profile/edit-profile.page.ts
+++ src/app/edit-profile/edit-profile.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/user.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.page.scss'],
 })
 export class EditProfilePage implements OnInit {
+  userData: any = {};
+  newPassword: string = '';
+  confirmPassword: string = '';
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.userData = await this.userService.getUserData();
   }
 
+  async saveChanges() {
+    if (this.newPassword !== this.confirmPassword) {
+      await this.showAlert('Error', 'Las contraseñas no coinciden.');
+      return;
+    }
+
+    // Actualizar los datos del usuario
+    if (this.newPassword) {
+      this.userData.password = this.newPassword;
+    }
+
+    await this.userService.updateUserData(this.userData);
+    await this.showAlert('Éxito', '¡Perfil actualizado correctamente!');
+    this.router.navigate(['/profile']);
+  }
+
+  cancelEdit() {
+    this.router.navigate(['/profile']);
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 }
