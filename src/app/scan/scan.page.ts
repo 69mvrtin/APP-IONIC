dde9d8c1feb70e58da212c1503185a50e3b97895
Ch3l0s <ma.boetto@duocuc.cl>
2024-11-28 00:01:01 -0300

    scan qr

--- src/app/scan/scan.page.ts
+++ src/app/scan/scan.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, Platform } from '@ionic/angular';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component'; // Ajusta la ruta según sea necesario
+import { LensFacing, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-scan',
@@ -6,12 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scan.page.scss'],
 })
 export class ScanPage implements OnInit {
+  segment = 'scan';
+  qrText = 'Confirma Tu Asistencia';
+  scannedCode = '';
 
-  segment = 'generate';
-  qrText ='Confirma Tu Asistencia';
-  constructor() { }
+  constructor(
+    private modalController: ModalController,
+    private platform: Platform // Inyectamos Platform
+  ) {}
 
-  ngOnInit() {
+  async starscan() {
+    const modal = await this.modalController.create({
+      component: BarcodeScanningModalComponent,
+      cssClass: 'barcode-scanning-modal',
+      showBackdrop: false,
+      componentProps: {
+        formats: [],
+        LensFacing: LensFacing.Back,
+      },
+    });
+
+    await modal.present();
+
+    const { data } = await modal.onWillDismiss();
+
+    if (data) {
+      this.scannedCode = data?.barcode?.displayValue;
+    }
   }
 
+  ngOnInit(): void {
+    if (this.platform.is('capacitor')) {
+      BarcodeScanner.isSupported().then((supported) => {
+        console.log('Scanner supported:', supported);
+      });
+
+      BarcodeScanner.checkPermissions().then((permissions) => {
+        console.log('Permissions:', permissions);
+      });
+      BarcodeScanner.removeAllListeners();
+    }
+  }
 }
