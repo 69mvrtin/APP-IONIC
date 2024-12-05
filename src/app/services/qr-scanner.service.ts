04206b8d4dcc0e113036e07f52bb8ac99e63a8dc
LUIS MARTIN SEPULVEDA LAGOS <lm.sepulveda@duocuc.cl>
2024-12-04 22:04:12 -0300

    qrcode

--- src/app/services/qr-scanner.service.ts
+++ src/app/services/qr-scanner.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QrScannerService {
+  private barcodeScannerSupported = false;
 
-  constructor() { }
+  constructor() {}
+
+  async init(): Promise<void> {
+    try {
+      const result = await BarcodeScanner.isSupported();
+      this.barcodeScannerSupported = result.supported;
+    } catch (e) {
+      console.error('Error al verificar soporte de escáner:', e);
+    }
+  }
+
+  async scan(): Promise<string[]> {
+    if (this.barcodeScannerSupported) {
+      if (await this.requestPermissions()) {
+        console.log('Iniciando escaneo, abriendo cámara...'); // Mensaje para verificar
+        const { barcodes } = await BarcodeScanner.scan();
+        return barcodes.map((barcode) => barcode.rawValue);
+      } else {
+        console.warn('Permisos de cámara denegados.');
+        return [];
+      }
+    } else {
+      console.error('Escáner no soportado.');
+      return [];
+    }
+  }
+  
+
+  private async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
 }
+
+
